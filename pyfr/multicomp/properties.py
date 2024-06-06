3c19430a4dd0121987f141c23e18a553f249899f
Kyle A. Schau <kaschau89@gmail.com>
2024-06-06 09:46:57 -0400

    Added new mc solvers as copy of non mc

---
+++ pyfr/multicomp/properties.py
@@ -0,0 +1,24 @@
+from os import path
+
+class BaseProperties:
+    def __init__(self, path_or_list):
+
+        if path.exists(path_or_list):
+            pass
+        else:
+            self.species_names = path_or_list.split(",")
+
+        self.ns = len(self.species_names)
+        self.properties = dict()
+
+class ThermoProperties(BaseProperties):
+    def __init__(self, path_or_list):
+        super().__init__()
+
+class TransportProperties(BaseProperties):
+    def __init__(self, path_or_list):
+        super().__init__()
+
+class ChemistyProperties(ThermoProperties):
+    def __init__(self, path_or_list):
+        super().__init__()
--- pyfr/solvers/__init__.py
+++ pyfr/solvers/__init__.py
@@ -4,6 +4,8 @@ from pyfr.solvers.acnavstokes import ACNavierStokesSystem
 from pyfr.solvers.base import BaseSystem
 from pyfr.solvers.euler import EulerSystem
 from pyfr.solvers.navstokes import NavierStokesSystem
+from pyfr.solvers.mceuler import MCEulerSystem
+from pyfr.solvers.mcnavstokes import MCNavierStokesSystem
 from pyfr.util import subclass_where
 
 
