fixed temperature derivative calculation for multicomp.

pyfr/multicomp/eos/tpgEOS.py
@@ -42,24 +42,17 @@ class tpgEOS(BaseEOS):
 
         # Compute h
         h = 0.0
-        T2o2 = T*T / 2.0
-        T3o3 = T**3 / 3.0
-        T4o4 = T**4 / 4.0
-        T5o5 = T**5 / 5.0
         Ru = consts['Ru']
         MW = consts['MW']
-        N7 = consts['NASA7']
+        N7 = consts['NASA7'] * Ru/MW[:, np.newaxis]
         for n, Y in enumerate(pris[ndims+2::]+[Yns]):
             m = np.where(T <= N7[n,0], 8, 1)
-            hns = (
-                N7[n, m + 0] * T
-                + N7[n, m + 1] * T2o2
-                + N7[n, m + 2] * T3o3
-                + N7[n, m + 3] * T4o4
-                + N7[n, m + 4] * T5o5
-                + N7[n, m + 5]
-            ) * Ru/MW[n]
-            h += hns * Y
+            h += (  T*(N7[n, m + 0]
+                   + T*(N7[n, m + 1] / 2.0
+                   + T*(N7[n, m + 2] / 3.0
+                   + T*(N7[n, m + 3] / 4.0
+                   + T*(N7[n, m + 4] / 5.0))))) + N7[n, m + 5]) * Y
+
         # Compute density
         rho = p/(Rmix*T)
 
@@ -103,36 +96,27 @@ class tpgEOS(BaseEOS):
         N7 = consts['NASA7'] * consts['Ru'] / consts['MW'][:, np.newaxis]
         N7[:,0] = consts['NASA7'][:,0]
         # Iterate on T, start at 300K
-        T = np.ones(rho.shape)*3000.0
+        T = np.ones(rho.shape)*300.0
         error = np.ones(rho.shape)
         niter = 0
         tol = 1e-8
         while np.max(np.abs(error)) > tol and niter < 100:
             h = 0.0
             cp = 0.0
-            T2 = T*T
-            T3 = T2*T
-            T4 = T3*T
-            T5 = T4*T
             for n, Y in enumerate(Yk+[Yns]):
                 m = np.where(T <= N7[n,0], 8, 1)
-                cps = (
-                    N7[n, m + 0]
-                    + N7[n, m + 1] * T
-                    + N7[n, m + 2] * T2
-                    + N7[n, m + 3] * T3
-                    + N7[n, m + 4] * T4
-                )
-                hns = (
-                    N7[n, m + 0] * T
-                    + N7[n, m + 1] * T2/2.0
-                    + N7[n, m + 2] * T3/3.0
-                    + N7[n, m + 3] * T4/4.0
-                    + N7[n, m + 4] * T5/5.0
-                    + N7[n, m + 5]
-                )
-                cp += cps * Y
-                h += hns * Y
+                cp += (     N7[n, m + 0]
+                       + T*(N7[n, m + 1]
+                       + T*(N7[n, m + 2]
+                       + T*(N7[n, m + 3]
+                       + T*(N7[n, m + 4] ))))) * Y
+
+                h += (  T*(N7[n, m + 0]
+                       + T*(N7[n, m + 1] /2.0
+                       + T*(N7[n, m + 2] /3.0
+                       + T*(N7[n, m + 3] /4.0
+                       + T*(N7[n, m + 4] /5.0)))))
+                       +    N7[n, m + 5]) * Y
             error = e - (h - Rmix * T)
             # Newtons Method
             T = T - error / (-cp - Rmix)