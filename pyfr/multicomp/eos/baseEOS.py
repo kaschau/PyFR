8940bd91e35efc708b155caf6a56decade3fddff
Kyle A. Schau <kaschau89@gmail.com>
2024-07-10 13:52:07 -0400

    chaining Ys in pris<->cons and added validation to ICs species mass.

--- pyfr/multicomp/eos/baseEOS.py
+++ pyfr/multicomp/eos/baseEOS.py
@@ -1,7 +1,14 @@
-
+import numpy as np
 
 class BaseEOS:
     name = None
 
     def __init__(self):
         pass
+
+    def validate_Y_ics(self, Yk):
+        for Y in Yk:
+            if np.max(Y) > 1.0:
+                raise ValueError('Species mass > 1.0 detected in ICs')
+            elif np.min(Y) < 0.0:
+                raise ValueError('Species mass < 0.0 detected in ICs')
