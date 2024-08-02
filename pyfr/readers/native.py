232723703d2d32c7d38ad8bae76ab206816db6ec
Freddie Witherden <freddie@witherden.org>
2024-08-02 09:41:36 -0500

    Improve solution metadata.

--- pyfr/readers/native.py
+++ pyfr/readers/native.py
@@ -66,7 +66,7 @@ class NativeReader:
         with h5py.File(sname, 'r') as f:
             if rank == root:
                 # Ensure the solution is from the mesh we are using
-                uuid = f['mesh_uuid'][()].decode()
+                uuid = f['mesh-uuid'][()].decode()
                 if uuid != self.mesh.uuid:
                     raise RuntimeError('Invalid solution for mesh')
 
@@ -150,7 +150,7 @@ class NativeReader:
         if rank == root:
             creator = self.f['creator'][()].decode()
             codec = [c.decode() for c in self.f['codec']]
-            uuid = self.f['mesh_uuid'][()].decode()
+            uuid = self.f['mesh-uuid'][()].decode()
             version = self.f['version'][()]
 
             meta = (creator, codec, uuid, version)
