7e53dd370519a17b095585a19996564cdfe677a0
Kyle A. Schau <kaschau89@gmail.com>
2024-11-06 11:48:47 -0500

    fitting cp, integrate cp to get h, integrate cp/T to get s

--- pyfr/multicomp/eos/tpg.py
+++ pyfr/multicomp/eos/tpg.py
@@ -27,32 +27,56 @@ class tpgEOS(BaseEOS):
         prop_calc = self.cfg.get('multi-component', 'property-calc', 'strict')
         if prop_calc == 'strict':
             self.consts['NASA7'] = self.input_props['NASA7']
-        else:
+        elif prop_calc == 'fast':
             N7 = self.input_props['NASA7']
             Tmin = self.cfg.getfloat('multi-component', 'T-min', 300.0)
-            Tmax = self.cfg.getfloat('multi-component', 'T-max', 5000.0)
+            Tmax = self.cfg.getfloat('multi-component', 'T-max', 3500.0)
             Ts = np.linspace(Tmin, Tmax, 500)
 
             self.consts['NASA7'] = np.empty((self.consts['ns'], 7))
             for n in range(self.consts['ns']):
+
                 m = np.where(Ts <= N7[n, 0], 8, 1)
 
-                # Fit h
-                h = (  Ts*(N7[n, m + 0]
-                     + Ts*(N7[n, m + 1] / 2.0
-                     + Ts*(N7[n, m + 2] / 3.0
-                     + Ts*(N7[n, m + 3] / 4.0
-                     + Ts*(N7[n, m + 4] / 5.0))))) + N7[n, m + 5])
-                h_poly = np.polynomial.Polynomial.fit(Ts, h, 5)
+                # Fit cp
+                cp = (       N7[n, m + 0]
+                       + Ts*(N7[n, m + 1]
+                       + Ts*(N7[n, m + 2]
+                       + Ts*(N7[n, m + 3]
+                       + Ts*(N7[n, m + 4] )))))
 
-                coeffs = h_poly.convert().coef
-                coeffs *= [1, 1, 2, 3, 4, 5]
-                # reorder so a0 is first
-                dque = deque(coeffs)
-                dque.rotate(-1)
-                coeffs = list(dque)
+                cp_poly = np.polynomial.Polynomial.fit(Ts, cp, 4)
+                coeffs = list(cp_poly.convert().coef)
 
-                # solve for a6 (entorpy)
+                #import matplotlib.pyplot as plt
+                # plt.plot(Ts, cp, label="ref")
+                # cp_new = (   coeffs[0]
+                #        + Ts*(coeffs[1]
+                #        + Ts*(coeffs[2]
+                #        + Ts*(coeffs[3]
+                #        + Ts*(coeffs[4] )))))
+                # plt.plot(Ts, cp_new, '--', label="new")
+                # plt.legend()
+                # plt.show()
+
+                h  = (  Ts*(N7[n, m + 0]
+                      + Ts*(N7[n, m + 1] / 2.0
+                      + Ts*(N7[n, m + 2] / 3.0
+                      + Ts*(N7[n, m + 3] / 4.0
+                      + Ts*(N7[n, m + 4] / 5.0))))) + N7[n, m + 5])
+                h_new  = (  Ts*(coeffs[0]
+                          + Ts*(coeffs[1] / 2.0
+                          + Ts*(coeffs[2] / 3.0
+                          + Ts*(coeffs[3] / 4.0
+                          + Ts*(coeffs[4] / 5.0))))))
+                a5 = np.mean(h - h_new)
+                coeffs.append(a5)
+                # plt.plot(Ts, h, label="ref")
+                # plt.plot(Ts, h_new + a5, "--", label="new")
+                # plt.legend()
+                # plt.show()
+
+                # entropy integration constant
                 s = (  np.log(Ts)*N7[n, m + 0]
                             +(Ts *(N7[n, m + 1]
                             + Ts *(N7[n, m + 2] / 2.0
@@ -60,24 +84,31 @@ class tpgEOS(BaseEOS):
                             + Ts *(N7[n, m + 4] / 4.0))))) + N7[n, m + 6])
 
                 s_new = (  np.log(Ts)*coeffs[0]
-                            +(Ts *(coeffs[1]
-                            + Ts *(coeffs[2] / 2.0
-                            + Ts *(coeffs[3] / 3.0
-                            + Ts *(coeffs[4] / 4.0))))))
-
-                def f(a6):
-                    return np.sum((s - (s_new + a6))**2)
-
-                a6 = minimize(f, N7[n, 7]).x[0]
-
+                               + (Ts*(coeffs[1]
+                               +  Ts*(coeffs[2] / 2.0
+                               +  Ts*(coeffs[3] / 3.0
+                               +  Ts*(coeffs[4] / 4.0))))))
+                a6 = np.mean(s - s_new)
                 coeffs.append(a6)
+                # plt.plot(Ts, s, label="ref")
+                # plt.plot(Ts, s_new + a6, "--", label="new")
+                # plt.legend()
+                # plt.show()
+
                 self.consts['NASA7'][n, :] = coeffs
+        else:
+            print(type(prop_calc))
+            raise ValueError(f'Unknown property-calc method "{prop_calc}".')
 
 
     def pri_to_con(self, pris):
         consts = self.consts
+        Ru = consts['Ru']
+        MW = consts['MW']
+        NASA7 = consts['NASA7']
         ns = consts['ns']
         ndims = len(pris) - (ns - 1) - 2
+
         p, T = pris[0], pris[ndims + 1]
 
         # Compute ns species
@@ -94,22 +125,21 @@ class tpgEOS(BaseEOS):
 
         # Compute h
         h = 0.0
-        Ru = consts['Ru']
-        MW = consts['MW']
-        N7 = consts['NASA7'] * Ru/MW[:, np.newaxis]
-        N7[:, 0] = consts['NASA7'][:, 0]
         for n, Y in enumerate(it.chain(pris[ndims+2::],[Yns])):
-            if len(N7[n]) == 15: # strict
-                m = np.where(T <= N7[n,0], 8, 1)
-            elif len(N7[n]) == 7:
+            N7 = NASA7[n]
+            if len(N7) == 15: # strict
+                m = np.where(T <= N7[0], 8, 1)
+                N7[1::] *= Ru/MW[n]
+            elif len(NASA7[n]) == 7:
                 m = 0
+                N7 *= Ru/MW[n]
             else:
                 raise ValueError("NASA7 Issue.")
-            h += (  T*(N7[n, m + 0]
-                  + T*(N7[n, m + 1] / 2.0
-                  + T*(N7[n, m + 2] / 3.0
-                  + T*(N7[n, m + 3] / 4.0
-                  + T*(N7[n, m + 4] / 5.0))))) + N7[n, m + 5]) * Y
+            h += (  T*(N7[m + 0]
+                  + T*(N7[m + 1] / 2.0
+                  + T*(N7[m + 2] / 3.0
+                  + T*(N7[m + 3] / 4.0
+                  + T*(N7[m + 4] / 5.0))))) + N7[m + 5]) * Y
 
         # Compute density
         rho = p/(Rmix*T)
@@ -129,6 +159,9 @@ class tpgEOS(BaseEOS):
 
     def con_to_pri(self, cons):
         consts = self.consts
+        Ru = consts['Ru']
+        MW = consts['MW']
+        NASA7 = consts['NASA7']
         ns = consts['ns']
         ndims = len(cons)-(ns-1)-2
 
@@ -144,14 +177,12 @@ class tpgEOS(BaseEOS):
         # Compute mixture properties
         Rmix = 0.0
         for n, Y in enumerate(Yk):
-            Rmix += Y / consts["MW"][n]
-        Rmix *= consts["Ru"]
+            Rmix += Y / MW[n]
+        Rmix *= Ru
 
         # Internal energu
         e = rhoE/rho - 0.5 * sum(v * v for v in vs)
 
-        N7 = consts['NASA7'] * consts['Ru'] / consts['MW'][:, np.newaxis]
-        N7[:,0] = consts['NASA7'][:,0]
         # Iterate on T, start at 300K
         T = np.ones(rho.shape)*300.0
         error = np.ones(rho.shape)
@@ -161,24 +192,28 @@ class tpgEOS(BaseEOS):
             h = 0.0
             cp = 0.0
             for n, Y in enumerate(Yk):
-                if N7[n].shape[0] == 15: # strict
-                    m = np.where(T <= N7[n,0], 8, 1)
-                elif N7[n].shape == 7:
+                N7 = NASA7[n]
+                if len(N7) == 15: # strict
+                    m = np.where(T <= N7[0], 8, 1)
+                    N7[1::] *= Ru/MW[n]
+                elif len(NASA7[n]) == 7:
                     m = 0
+                    N7 *= Ru/MW[n]
                 else:
                     raise ValueError("NASA7 Issue.")
-                cp += (     N7[n, m + 0]
-                       + T*(N7[n, m + 1]
-                       + T*(N7[n, m + 2]
-                       + T*(N7[n, m + 3]
-                       + T*(N7[n, m + 4] ))))) * Y
 
-                h += (  T*(N7[n, m + 0]
-                      + T*(N7[n, m + 1] /2.0
-                      + T*(N7[n, m + 2] /3.0
-                      + T*(N7[n, m + 3] /4.0
-                      + T*(N7[n, m + 4] /5.0)))))
-                      +    N7[n, m + 5]) * Y
+                cp += (     N7[m + 0]
+                       + T*(N7[m + 1]
+                       + T*(N7[m + 2]
+                       + T*(N7[m + 3]
+                       + T*(N7[m + 4] ))))) * Y
+
+                h += (  T*(N7[m + 0]
+                      + T*(N7[m + 1] /2.0
+                      + T*(N7[m + 2] /3.0
+                      + T*(N7[m + 3] /4.0
+                      + T*(N7[m + 4] /5.0)))))
+                      +    N7[m + 5]) * Y
             error = e - (h - Rmix * T)
             # Newtons Method
             T = T - error / (-cp - Rmix)
